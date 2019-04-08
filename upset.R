f92a632357a47e4c870a521f660fe2f5165c78cd
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-08 13:59:43 -0700

    Update upset.R
    adds a condition to check if any column in the data frame is logical data type
    LIMITATION: It checks every entry for logical data, but it only needs to check any one non-null value per column.

--- upset.R
+++ upset.R
@@ -5,12 +5,19 @@ table_column_upset <- function(data, pdf=NULL, width=NA, height=NA, nsets=NA, ..
   ncols <- dim(data)[2]
   if (ncols <= 0) { return() }
   for (i in 1:ncols) {
-  
 
-#  data[,i] <- as.numeric(data[,i])
-# removing the above line will allow the function to assign presence/absence for any data type 
+#test for boolean data and convert "true" to 1 and "false" to 0 if data is boolean 
+#I don't know if SQL boolean data will come into R as logical or character.  Assuming logical.
+#If it were the FALSE values that were important to know about, FALSE should be assigned 1 and TRUE 0 (no change for is.na).
+
+if (is.logical(data[,i])) == TRUE {
+	data[which(data[,i] == TRUE), i] <- 1
+	data[which(data[,i] == FALSE, i] <- 0
+	data[which(is.na(data[,i])),i] <- 0
+}
 
 
+#below=original code to convert numeric data to binary values
     data[which(!is.na(data[,i])),i] <- 1
     data[which(is.na(data[,i])),i] <- 0
   }

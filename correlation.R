09797ca79bfca977ae621bdb5e7f0a1165bb04e0
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-10 17:21:19 -0700

    first draft descriptive stats
    no methods, no error handling yet!
    small collection of descriptive tools to analyze numeric datasets
    -currently hard coded with 2 vectors

---
+++ correlation.R
@@ -0,0 +1,63 @@
+#data from Principles of Biostatistics, page 407 (table 17.3)
+#data represents percentage of births attended by trained health care personnel and maternal mortality rates for 20 countries
+
+#create vectors to use in analyses
+attendedBirth <- c(5,100,98,84,100,99,70,50,26,6,100,37,35,96,55,90,96,99,99,95)
+maternalMortality <- c(600,3,67,170,6,15,120,170,300,830,10,800,500,60,100,10,5,5,8,120)
+
+#create data frame
+birthMortality <- data.frame(attendedBirth, maternalMortality)
+
+#later: add functionality to accept input other than these two vectors
+
+#provide summary stats
+
+#calculate means
+#later: add variables to control parameters
+meanBirths <- mean(attendedBirth,weights=NULL,trim=0,na.rm=TRUE)
+meanMortality <- mean(maternalMortality,weights=NULL,trim=0,na.rm=TRUE)
+
+#calculate medians
+#later: add variables to control parameters
+medianBirths <- median(attendedBirth,weights=NULL,trim=0,na.rm=TRUE)
+medianMortality <- median(maternalMortality,weights=NULL,trim=0,na.rm=TRUE)
+
+#boxplot
+#later: add parameters for labels! and and more
+#In this instance (the hardcoded dataset), it doesn't make sense 
+#to do the boxplots for both vectors in one image.  However, it might
+#make sense in other cases.
+#later: add functionality to control how many sets of data might be
+#plotted together or separately (assume large dataset comes in threough
+#a SQL connection in a table with many rows and columns -- some columns may
+#be better suited to considering together
+#later: also provide values for ranges and quantiles
+boxplot(attendedBirth)
+boxplot(maternalMortality)
+boxplot(birthMortality)
+
+#simple scatter plot
+#can provide two vectors or 2-column vector frame
+#later:add functionality to handle other input and give parameters
+#later:add best fit line
+plot(attendedBirth,maternalMortality)
+
+#correlation by pearson
+pearsonCorr <- cor.test(x=attendedBirth, y=maternalMortality, method = 'pearson')
+pearsonT <- pearsonCorr$statistic
+pearsonP <- pearsonCorr$p.value
+pearsonCI <- pearsonCorr$conf.int
+
+#correlation by spearman
+#later: add methods for user to choose correlation method?
+spearmanCorr <- cor.test(x=attendedBirth, y=maternalMortality, method = 'spearman')
+spearmanT <- spearmanCorr$statistic
+spearmanP <- spearmanCorr$p.value
+
+#add assessment of missingness
+#add assessment of outliers/unusual values
+#eventually format to give all (or desired part) info in single
+#document of descriptive stats about given table/data
+
+
+

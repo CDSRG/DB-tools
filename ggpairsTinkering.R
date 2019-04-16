5b22d93c26a39eeceeb85ffb4db658b1dfaee526
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-16 16:19:49 -0700

    inserted user-defined function
    It goes in!
    Issues:
    The "upper" and "lower" parameters include the categorical graphs along the right side and bottom of the graph matrix.
    The type is too big on my inserted graph.

--- ggpairsTinkering.R
+++ ggpairsTinkering.R
@@ -2,26 +2,20 @@
 
 library(ggplot2)
 library(GGally)
+library(ggcorrplot)
 
 data(iris)
 
-#one example of basic use of ggpairs()
-
-ggpairs(iris, upper = list(continuous = "points", combo = "box",discrete = "facetbar", na = "na"), 
-lower = list(continuous = "cor", combo = "facethist", discrete = "facetbar", na = "na"), 
-diag = list(continuous = "densityDiag", discrete = "barDiag", na = "naDiag"))
-
-
+#compute a correlation matrix
+corr <- cor(iris[,1:4])
+#compute a correlation p-values matrix
+p.mat <- cor_pmat(iris[,1:4])
 
 #attempt to put a user-designed function into ggpairs
 
 #develop function to insert
 #great assistance from the examples in ggcorrplot documentation
-library(ggcorrplot)
-#compute a correlation matrix
-corr <- cor(iris[,1:4])
-#compute a correlation p-values matrix
-p.mat <- cor_pmat(iris[,1:4])
+
 #visualize the correlation matrix
 ggcorrplot(corr)
 #reorder the matrix
@@ -30,3 +24,17 @@ ggcorrplot(corr, hc.order = TRUE, outline.col = "white")
 ggcorrplot(corr, hc.order = TRUE, type = "lower", outline.col = "white")
 #add correlation coefficients and pvalues, leaving blank if not significant
 ggcorrplot(corr, p.mat = p.mat, hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
+
+
+#prepare ggpairs framework to take the user-designed function
+customPairs <- ggpairs(iris, upper = "blank")
+#note: "blank" removed the categorical graphs at the edge too
+#maybe we can get them back in later
+#assign user-designed function to variable
+customPlot <- ggcorrplot(corr, p.mat = p.mat, hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
+#place plot into ggpairs matrix
+customPairs[2,4] <- customPlot
+#call
+customPairs
+
+

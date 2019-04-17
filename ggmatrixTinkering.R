8d0d406c38f8cfee58261575e1ac2791a88229b5
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-17 14:32:11 -0700

    trying to label individual plots
    this is not good
    1 - geom_text leaves the grid in the background
    2 - can't control the size of individual plots
    
    I think ggmatrix would be terrific in situations that only need overall row and column labels.  For creative control, it seems we will need layout instead.

--- ggmatrixTinkering.R
+++ ggmatrixTinkering.R
@@ -5,26 +5,38 @@ require(ggcorrplot)
 ### first plot
 plot1 <- ggplot(iris, aes(Petal.Length, Petal.Width)) + geom_point() + geom_quantile()
 
+### legend for first plot
+plotLegend1 <- ggally_text("Scatterplot with quantile lines")
+
 ### second plot
 plot2 <- ggcorrplot(cor(iris[,1:4]), p.mat = cor_pmat(iris[,1:4]), hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
+
+### legend for second plot
+plotLegend2 <- ggally_text("Correlation matrix with p-values")
 
 ### third plot
 plot3 <- ggplot(iris, aes(x=Sepal.Length)) + geom_histogram(aes(color=Species),fill="white",position="dodge")
 
+### legend for third plot
+plotLegend3 <- ggally_text("Histogram")
+
 ### fourth plot
 plot4 <- ggplot(iris, aes(x=Petal.Length, y=Petal.Width)) + geom_point(aes(color=Species, shape=Species)) + geom_smooth(aes(color=Species, shape=Species), method=lm, se=FALSE, fullRange=TRUE)
 
+### legend for fourth plot
+plotLegend4 <- ggally_text("Scatterplot by group")
+
 ### put plots in a list
-plotList <- list(plot1, plot2, plot3, plot4)
+plotList <- list(plot1, plot2, plotLegend1, plotLegend2, plot3, plot4, plotLegend3, plotLegend4)
 
 ### make matrix of plots from list
 plotMatrix <- ggmatrix(
   plotList,
-  2, 2,
-  xAxisLabels = c("a","b"), yAxisLabels = c("c","d"),
+  4, 2,
+  xAxisLabels = NULL, yAxisLabels = NULL,
   byrow = TRUE,
   showStrips = TRUE,
-  showAxisPlotLabels = TRUE, showXAxisPlotLabels = TRUE, showYAxisPlotLabels = TRUE,
+  showAxisPlotLabels = FALSE, showXAxisPlotLabels = FALSE, showYAxisPlotLabels = FALSE,
   title="test", xlab="x label", ylab="y label"
 )
 

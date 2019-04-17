4f49246a2ec0f7f2b96d063de3e1104dd07b5d1a
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-17 09:31:10 -0700

    beginning of trees from help
    All it does now is install and make trees with the iris and mtcars datasets.

--- help trees.R
+++ help trees.R
@@ -1,5 +1,11 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("HELP", version = "3.8")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+# BiocManager::install("HELP", version = "3.8")
 
 require("HELP")
+
+data(iris)
+plotPairs(as.matrix(iris[,1:4]))
+
+data(mtcars)
+plotPairs(as.matrix(mtcars))

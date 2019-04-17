420afad9dfee0525dcb26d2e7071e8a4a9a89390
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-04-17 08:04:33 -0700

    added a second plot
    I gave it a second user-defined function and did a little tidying.  I did leave the step-wise adjustments of the correlation matrix visualization in (but commented) for a reference.

--- ggpairsTinkering.R
+++ ggpairsTinkering.R
@@ -15,25 +15,32 @@ p.mat <- cor_pmat(iris[,1:4])
 
 #develop function to insert
 #great assistance from the examples in ggcorrplot documentation
-
 #visualize the correlation matrix
-ggcorrplot(corr)
+#ggcorrplot(corr)
 #reorder the matrix
-ggcorrplot(corr, hc.order = TRUE, outline.col = "white")
+#ggcorrplot(corr, hc.order = TRUE, outline.col = "white")
 #take just lower triangle
-ggcorrplot(corr, hc.order = TRUE, type = "lower", outline.col = "white")
+#ggcorrplot(corr, hc.order = TRUE, type = "lower", outline.col = "white")
 #add correlation coefficients and pvalues, leaving blank if not significant
 ggcorrplot(corr, p.mat = p.mat, hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
 
+#give it a second function to insert
+xAxis <- iris$Petal.Length
+yAxis <- iris$Petal.Width
+ggplot(iris, aes(xAxis, yAxis)) + geom_point() + geom_quantile()
 
 #prepare ggpairs framework to take the user-defined function
 customPairs <- ggpairs(iris, upper = "blank")
 #note: "blank" removed the categorical graphs at the edge too
 #maybe we can get them back in later?
-#assign user-designed function to variable
-customPlot <- ggcorrplot(corr, p.mat = p.mat, hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
-#place plot into ggpairs matrix
-customPairs[2,4] <- customPlot
+#assign first user-defined function to variable
+customPlot1 <- ggcorrplot(corr, p.mat = p.mat, hc.order = TRUE, type= "lower", lab = TRUE, insig = "blank")
+#place first plot into ggpairs matrix
+customPairs[2,4] <- customPlot1
+#assign second user-defined function to variable
+customPlot2 <- ggplot(iris, aes(xAxis, yAxis)) + geom_point() + geom_quantile()
+#place second plot into ggpairs matrix
+customPairs[1,3] <- customPlot2
 #call
 customPairs
 
---
+++ help trees.R
@@ -0,0 +1,5 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("HELP", version = "3.8")
+
+require("HELP")

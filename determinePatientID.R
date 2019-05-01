e7f55a242e692ced341a762acadc68008a878dd2
reidt03 <thompsre@ohsu.edu>
2019-05-01 10:58:14 -0700

    adds matching capabilities to buildSearchString

--- determinePatientID.R
+++ determinePatientID.R
@@ -11,20 +11,39 @@ if (!isNamespaceLoaded("RODBC")) {
 }
 
 # 
-buildSearchString <- function(targets = NULL) {
+buildSearchString <- function(targets = NULL, match=c("exact", "like", "exclude", "notlike")) {
+	# length(targets)==length(match) - or if length(targers)>1 and length(mathch)==1 -> then apply that same match type for all
+	#various error checking here :) for types fo values, etc., matches should only be one of exact, like, exclude
+
+	matches.exact <- which(match == "exact")
+	matches.exclude <- which(match == "exclude")
+	matches.like <- which(match == "like")
+	matches.notlike <- which(match == "notlike")
+	if (length(matches.exact) == 1) {
+		# COLUMN_NAME == 'targets[matches.exact]'
+	}
+	else if (length(matches.exact) > 1) {
+		# COLUMN_NAME IN ('target1','target2',...)
+	}
+
+	if (length(matches.like) >= 1) {
+		# COLUMN_NAME LIKE 'target1' OR COLUMN_NAME LIKE 'target2', ...
+	}
+	if (length(matches.exclude) == 1) {
+		# COLUMN_NAME !=target
+	}
+	else if (length(matches.exclude) > 1) {
+		# COLUMN_NAME NOT IN ('targe1','target2',...)
+	}
+	if (length(matches.notlike) >= 1) {
+		# COLUMN_NAME NOT LIKE target1 AND COLUMN_NAME NOT LIKE target2, ....
+	}
+	# ((exact) OR (like)) AND ((exclude) OR (notlike))
+
 	searchString <- paste("(COLUMN_NAME LIKE ", paste("'", targets, "'", collapse=" OR COLUMN_NAME LIKE ", sep=""), ")", sep="")
 	return(searchString)
 }
-buildSearchString <- function(target1 = NULL, target2 = NULL, target3 = NULL, target4 = NULL, target5 = NULL) {
-	moreTargets <- c(target2, target3, target4, target5)
-	searchString <- paste("(COLUMN_NAME LIKE '", target1, "'", sep = 0)
-	for (i in 1:length(moreTargets)) { 
-		searchString <- paste(searchString, " OR COLUMN_NAME LIKE '", moreTargets[i], "'", sep = 0)
-	}
-#use sapply instead?
-	searchString <- paste(searchString, ")")
-	return(searchString)
-}
+
 #possible to accept unknown number of parameters?
 
 restrictDataType <- function(type1 = NULL, type2, = NULL, type3 = NULL) {

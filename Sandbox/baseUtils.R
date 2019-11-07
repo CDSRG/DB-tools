7f30dac984a6ce951e57e97a33ec602ef87a538b
reidt03 <thompsre@ohsu.edu>
2019-11-06 21:57:58 -0800

    makes whereConstruct() generic for any columns

--- Sandbox/baseUtils.R
+++ Sandbox/baseUtils.R
@@ -92,114 +92,176 @@ fetchQuery <- function(con, n=NULL, buffsize=1000, verbose=FALSE, keep=FALSE, FU
 
 ###  CAN MULTIPLE CONNECTIONS RUN AT SAME TIME FROM R?!?!?  CAN THEN USE MULTIPLE CONNECTIONS TO GET PARTS OF RESULTS SIMULTANEOUSLY??
 
-ICD9 <- c("99.%", "105.4", "531-534.999", "601-603.4")
-ICD10 <- c("K25-K28.999")
+#ICD9 <- c("99.%", "105.4", "531-534.999", "601-603.4")
+#ICD10 <- c("K25-K28.999")
+#whereConstruct(columns=list(ICD9code=ICD9, ICD10code=ICD10))
 
-temp <- function(ICD9=NULL, ICD10=NULL, ICD9field="ICD9code", ICD10field="ICD10code") {
-	ICD9.len <- length(ICD9)
-	if (ICD9.len > 0) {
-		ICD9.range <- grep("[-]", ICD9)
-		range.len <- length(ICD9.range)
-		ICD9.like <- setdiff(grep("[%]", ICD9), ICD9.range)
-		like.len <- length(ICD9.like)
-		exact.len <- ICD9.len - (range.len + like.len)
-		if (exact.len > 0) {
-			ICD9.exact <- setdiff(1:ICD9.len, c(ICD9.range, ICD9.like))
-		}
-		if (exact.len == 1) {
-			queryString.exact <- paste(ICD9field, " == '", ICD9[ICD9.exact], "'", sep="")
-		}
-		else if (exact.len > 1) {
-			queryString.exact <- paste(ICD9field, " IN (",
-				paste("'", ICD9[ICD9.exact], "'", collapse=",", sep=""),
-					")", sep=""
+
+whereConstruct <- function(columns=list(), dates=NULL, required=NULL) {
+	if (!is.list(columns) | length(columns) < 1) {
+		return("")
+	}
+	fields <- names(columns)
+	n <- length(fields)
+	if (n < 1) {
+		return("")
+	}
+	if (is.null(required) | all(is.na(required))) {
+		required.fields <- rep(FALSE, n)
+	}
+	else if (identical(required, TRUE) | identical(required, "*")) {
+		required.fields <- rep(TRUE, n)
+	}
+	else {
+		required.fields <- fields %in% required
+	}
+	strings <- list()
+	for (i in 1:n) {
+		field <- fields[i]
+		vals <- columns[[i]]
+		vals.len <- length(vals)
+		if (vals.len > 0) {
+			vals.not <- grep("^[!]", vals)
+#			vals.include <- setdiff(1:vals.len, vals.not)
+			not.len <- length(vals.not)
+			if (not.len > 0) {
+				vals <- sub("^[!]", "", vals)
+			}
+			vals.range <- grep(".+[-].+", vals)
+			vals.nrange <- intersect(vals.range, vals.not)
+			vals.range <- setdiff(vals.range, vals.not)
+			range.len <- length(vals.range)
+			nrange.len <- length(vals.nrange)
+			vals.like <- setdiff(grep("[%]", vals), c(vals.range, vals.nrange))
+			vals.nlike <- intersect(vals.like, vals.not)
+			vals.like <- setdiff(vals.like, vals.nlike)
+			like.len <- length(vals.like)
+			nlike.len <- length(vals.nlike)
+			exact.len <- vals.len - (range.len + like.len + not.len)
+			if (exact.len > 0) {
+				vals.exact <- setdiff(1:vals.len, c(vals.range, vals.like, vals.not))
+				vals.null <- which(vals[vals.exact] == "NULL")
+				vals.exact <- setdiff(vals.exact, vals.null)
+				null.len <- length(vals.null)
+				exact.len <- exact.len - null.len
+			}
+			else {
+				null.len <- 0
+			}
+			exclude.len <- not.len - (nrange.len + nlike.len)
+			if (exclude.len > 0) {
+				vals.exclude <- setdiff(vals.not, c(vals.nrange, vals.nlike))
+				vals.nnull <- which(vals[vals.exclude] == "NULL")
+				vals.exclude <- setdiff(vals.exclude, vals.nnull)
+				nnull.len <- length(vals.nnull)
+				exclude.len <- exclude.len - nnull.len
+			}
+			else {
+				nnull.len <- 0
+			}
+			if (null.len > 0) {
+				queryString.null <- paste(field, " IS NULL", sep="")
+			}
+			if (exact.len == 1) {
+				queryString.exact <- paste(field, " == '", vals[vals.exact], "'", sep="")
+			}
+			else if (exact.len > 1) {
+				queryString.exact <- paste(field, " IN (",
+					paste("'", vals[vals.exact], "'", collapse=",", sep=""),
+						")", sep=""
+					)
+			}
+			if (like.len >= 1) {
+				queryString.like <- paste(field, " LIKE '", vals[vals.like], "'", collapse=" OR ", sep="")
+			}
+			if (range.len >= 1) {
+				vals.range <- unlist(lapply(
+					strsplit(vals[vals.range],"-"), 
+					function(x) {
+						paste(" BETWEEN '", x[1], "' AND '",x[2], "'", sep="")
+					}
+				))
+				queryString.range <- paste(
+					if (range.len > 1) { "(" }, 
+					field, 
+					vals.range, 
+					if (range.len > 1) { ")" }, 
+					collapse=" OR ", sep=""
 				)
-		}
-		if (like.len >= 1) {
-			queryString.like <- paste(ICD9field, " LIKE '", ICD9[ICD9.like], "'", collapse=" OR ", sep="")
-		}
-		if (range.len >= 1) {
-			ICD9.range <- unlist(lapply(
-				strsplit(ICD9[ICD9.range],"-"), 
-				function(x) {
-					paste(" BETWEEN", x[1], "AND",x[2])
-				}
-			))
-			queryString.range <- paste(
-				if (range.len > 1) { "(" }, 
-				ICD9field, 
-				ICD9.range, 
-				if (range.len > 1) { ")" }, 
-				collapse=" OR ", sep=""
+			}
+			if (nnull.len > 0) {
+				queryString.nnull <- paste(field, " IS NOT NULL", sep="")
+			}
+			if (exclude.len == 1) {
+				queryString.exclude <- paste(field, " != '", vals[vals.exclude], "'", sep="")
+			}
+			else if (exclude.len > 1) {
+				queryString.exclude <- paste(field, " NOT IN (",
+					paste("'", vals[vals.exclude], "'", collapse=",", sep=""),
+	 				")", sep=""
+	 			)
+			}
+			if (nlike.len >= 1) {
+				queryString.nlike <- paste(field, " NOT LIKE '", 
+					vals[vals.nlike], "'", collapse=" AND ", sep=""
+				)
+			}
+			if (nrange.len >= 1) {
+				vals.nrange <- unlist(lapply(
+					strsplit(vals[vals.nrange],"-"), 
+					function(x) {
+						paste(" NOT BETWEEN '", x[1], "' AND '",x[2], "'", sep="")
+					}
+				))
+				queryString.nrange <- paste(
+					if (nrange.len > 1) { "(" }, 
+					field, 
+					vals.nrange, 
+					if (nrange.len > 1) { ")" }, 
+					collapse=" AND ", sep=""
+				)
+			}
+			strings[[i]] <- paste(if (vals.len > not.len) { "(" },
+				if (null.len > 0) { paste("(", queryString.null, ")", sep="") },
+				if ((null.len > 0) & (exact.len > 0)) { " OR " },				
+				if (exact.len > 0) { paste("(", queryString.exact, ")", sep="") },
+				if ((exact.len > 0) & (like.len > 0)) { " OR " },
+				if (like.len > 0) { paste("(", queryString.like, ")", sep="") },
+				if ((like.len > 0) & (range.len > 0)) { " OR "},
+				if (range.len > 0) { queryString.range },
+				if (vals.len > not.len) { ")" },
+				if ((vals.len > not.len) & (not.len > 0)) { " AND "},
+				if (nnull.len > 0) {paste("(", queryString.nnull, ")", sep="") },
+				if ((nnull.len > 0) & (exclude.len > 0)) { " AND " },				
+				if (exclude.len > 0) {paste("(", queryString.exclude, ")", sep="") },
+				if ((exclude.len > 0) & (nlike.len > 0)) { " AND " },
+				if (nlike.len > 0) { paste("(", queryString.nlike, ")", sep="") },
+				if ((nlike.len > 0) & (nrange.len > 0)) { " AND "},
+				if (nrange.len > 0) { queryString.nrange },
+				sep=""
 			)
 		}
-
-		ICD9String <- paste("(",
-			if (exact.len > 0) { paste("(", queryString.exact, ")", sep="") },
-			if ((exact.len > 0) & (like.len > 0)) { " OR " },
-			if (like.len > 0) { paste("(", queryString.like, ")", sep="") },
-			if ((like.len > 0) & (range.len > 0)) { " OR "},
-			if (range.len > 0) { queryString.range },
-			")",
-			sep=""
-		)
+		else {
+			strings[[i]] <- ""
+		}
 	}
-
-	ICD10.len <- length(ICD10)
-	if (ICD10.len > 0) {
-		ICD10.range <- grep("[-]", ICD10)
-		range.len <- length(ICD10.range)
-		ICD10.like <- setdiff(grep("[%]", ICD10), ICD10.range)
-		like.len <- length(ICD10.like)
-		exact.len <- ICD10.len - (range.len + like.len)
-		if (exact.len > 0) {
-			ICD10.exact <- setdiff(1:ICD10.len, c(ICD10.range, ICD10.like))
-		}
-		if (exact.len == 1) {
-			queryString.exact <- paste(ICD10field, " == '", ICD10[ICD10.exact], "'", sep="")
-		}
-		else if (exact.len > 1) {
-			queryString.exact <- paste(ICD10field, " IN (",
-				paste("'", ICD10[ICD10.exact], "'", collapse=",", sep=""),
-					")", sep=""
-				)
-		}
-		if (like.len >= 1) {
-			queryString.like <- paste(ICD10field, " LIKE '", ICD10[ICD10.like], "'", collapse=" OR ", sep="")
-		}
-		if (range.len >= 1) {
-			ICD10.range <- unlist(lapply(
-				strsplit(ICD10[ICD10.range],"-"), 
-				function(x) {
-					paste(" BETWEEN", x[1], "AND",x[2])
-				}
-			))
-			queryString.range <- paste(
-				if (range.len > 1) { "(" }, 
-				ICD10field, 
-				ICD10.range, 
-				if (range.len > 1) { ")" }, 
-				collapse=" OR ", sep=""
-			)
-		}
-		ICD10String <- paste("(",
-			if (exact.len > 0) { paste("(", queryString.exact, ")", sep="") },
-			if ((exact.len > 0) & (like.len > 0)) { " OR " },
-			if (like.len > 0) { paste("(", queryString.like, ")", sep="") },
-			if ((like.len > 0) & (range.len > 0)) { " OR "},
-			if (range.len > 0) { queryString.range },
-			")",
-			sep=""
-		)
-	}
-
-	ICDQuery <- paste("WHERE ",
-		if (ICD9.len > 0) { ICD9String },
-		if ((ICD9.len > 0) & (ICD10.len > 0)) { " OR " },
-		if (ICD10.len > 0) { ICD10String },
+	valid.strings <- unlist(lapply(strings, function(x) { x != "" }))
+	required.strings <- unlist(strings[required.fields & valid.strings])
+	required.len <- length(required.strings)
+	notrequired.strings <- unlist(strings[!required.fields & valid.strings])
+	notrequired.len <- length(notrequired.strings)
+	whereQuery <- paste("WHERE ",
+		if ((notrequired.len > 0) & (required.len > 0)) { "(" },
+		if (notrequired.len > 1) { "(" },
+		if (notrequired.len > 0) { paste(notrequired.strings, collapse=" OR ") },
+		if (notrequired.len > 1) { ")" },
+		if ((notrequired.len > 0) & (required.len > 0)) { " AND " },
+		if (required.len > 1) { "(" },
+		if (required.len > 0) { paste(required.strings, collapse=" AND ") },
+		if (required.len > 1) { ")" },
+		if ((notrequired.len > 0) & (required.len > 0)) { ")" },
 		sep=""
 	)
-
-	return(ICDQuery)
+	return(whereQuery)
 }
 

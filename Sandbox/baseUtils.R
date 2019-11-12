a9f70b55693b8bf212aa81125890d0ca6c45c95d
Clinical Data Science Research Group <49210407+CDSRG@users.noreply.github.com>
2019-11-12 15:23:40 -0800

    Update baseUtils.R
    adds comments to address

--- Sandbox/baseUtils.R
+++ Sandbox/baseUtils.R
@@ -94,6 +94,10 @@ fetchQuery <- function(con, n=NULL, buffsize=1000, verbose=FALSE, keep=FALSE, FU
 ### The only workable method I've found to run multiple connections is to open multiple instances of R, and they don't share stuff 
 ### (stuff = data, environment?).  So you could do it, but you'd have to save the results parts and read them into your main instance.
 
+############################
+############################
+### must test in sql -- is "NOT BETWEEN" valid? -- tend to think not
+### also what about > and <?  can we do those?
 
 
 #ICD9 <- c("99.%", "105.4", "531-534.999", "601-603.4")
